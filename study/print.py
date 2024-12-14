857b50a1104e704c2f9b0c04db4a9fbd381cbc91
lipewinning <felipealvesdesousa@gmail.com>
2024-12-14 21:08:31 +0000

    study

---
+++ study/print.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+
+print(34)
+
+print ("Hello World!" + str(34))
---
+++ study/variables.py
@@ -0,0 +1,9 @@
+
+name = 'Felipe'
+profession = 'writer'
+
+books_written = 6
+
+output = name + ' is a ' + profession + ' and he\'s written ' + str(books_written) + ' books.'
+
+print(output)
